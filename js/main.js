c8aa7ec57dc129a38c79bc8f48634c23e42f085d
junayet-sajib <junayet3@gmail.com>
2024-01-16 19:15:45 +0600

    Completed All Section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-$(document).ready(function(){
-    $('.navbar ul li a').click(function(){
-      $('li a').removeClass("active");
-      $(this).addClass("active");
+// select the things we need form the DOM
+var hamburger = document.querySelector("#hamburger");
+var nav = document.querySelector(".navMenu");
+var navLinks = document.querySelectorAll(".navMenu li");
+
+// toggle nav on click of hamburger menu icon
+hamburger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    // burger animation
+    hamburger.classList.toggle("toggle");
   });
-  });
+
+  // close nav by clicking on list items
+Array.from(navLinks).forEach((li) =>
+li.addEventListener("click", () => {
+  if (hamburger.classList.contains("toggle")) {
+    hamburger.classList.remove("toggle");
+  }
+  if (nav.classList.contains("nav-active")) {
+    nav.classList.remove("nav-active");
+  }
+})
+);
